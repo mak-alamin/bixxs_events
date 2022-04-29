2e5f3bb8939358a20b293db06655b228b395117f
Mak Alamin <makalamin012@gmail.com>
2022-04-29 12:16:14 +0600

    remove employee role first

--- includes/installer.php
+++ includes/installer.php
@@ -9,6 +9,7 @@ class Installer
 
     public function set_user_roles()
     {
+        remove_role('bixxs_event_employee');
         add_role('bixxs_event_employee', 'Mitarbeiter', get_role('shop_manager')->capabilities);
     }
 }
