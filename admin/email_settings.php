757250f8216ead8fdc382581d4007648b560ed93
Mak Alamin <makalamin012@gmail.com>
2022-06-27 12:02:29 +0600

    qr code send to email

--- admin/email_settings.php
+++ admin/email_settings.php
@@ -113,7 +113,38 @@ Ihre Freunde im Ticketshop Solutions Demo Shop</p>';
     require_once __DIR__ . '/views/email_settings/email_fields.php';
 }
 
-function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, $guest_email = '', $send_to_admin = true)
+/**
+ * Get QR Code Url
+ */
+function bixxs_events_email_setting_generate_qr_code_url($item_id, $guest_number, $ticket_template_id, $ticket_name, $order_date, $ticket_price, $veranstalter, $ort_veranstaltung, $qrcode_color, $guests)
+{
+    if (!class_exists('qrstr')) {
+        include_once plugin_dir_path(__FILE__) . 'phpqrcode-master/qrlib.php';
+    }
+    if (file_exists(plugin_dir_path(__FILE__) . 'phpqrcode-master/index.php') && class_exists('qrstr')) {
+        require plugin_dir_path(__FILE__) . 'phpqrcode-master/index.php';
+    }
+
+    $raw_svg = $qr_code_img[$guest_number - 1];
+
+    $image_path = '/phpqrcode-master/temp/' . md5($item_id) . '.png';
+
+    $image_file = __DIR__ . $image_path;
+
+    $img = new \Imagick();
+    $svg = file_get_contents($raw_svg);
+    $img->readImageBlob($svg);
+    $img->setImageFormat("png24");
+    $img->writeImage($image_file);
+    $img->clear();
+    $img->destroy();
+
+    $image_file = BIXXS_EVENTS_PLUGIN_DIR . substr($image_file, strpos($image_file, '/admin/phpqrcode-master/temp'));
+
+    return $image_file;
+}
+
+function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, $send_to_admin = true)
 {
     error_log(print_r($item, true));
     error_log($type);
@@ -126,9 +157,20 @@ function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, 
         return;
     }
 
-    $order = $item->get_order();
+    $guests = json_decode($item->get_meta('_mlx_guests'), true);
 
+    $guest_email = isset($guest['email']) ? $guest['email'] : '';
+
+    $product_id = $item->get_product_id();
+
+    $order = $item->get_order();
     $order_id = $order->get_id();
+
+    $ticket_name = $item->get_name();
+    $ticket_price = get_post_meta($product_id, '_regular_price', true);
+    $order_date = $order->get_date_created('Y/m/d')->format('d.m.Y');
+
+    $ticket_template_id = get_post_meta($product_id, 'bixxs_events_event_template', true);
 
     $first_name = $order->get_billing_first_name();
     $last_name = $order->get_billing_last_name();
@@ -143,6 +185,7 @@ function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, 
     if ($tick_result) {
         $veranstalter = trim($tick_result[0]->veranstalter);
         $ort_veranstaltung = trim($tick_result[0]->ort_veranstaltung);
+        $qrcode_color = trim($tick_result[0]->qrcode_color);
     } else {
         $veranstalter = '';
         $ort_veranstaltung = '';
@@ -161,6 +204,18 @@ function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, 
 
     $download_ticket_url = sprintf("%s?action=pdf_download&order_id=%s&item_id=%s&guest_number=$guest_number&ticket_number=%s&download_token=%s", site_url(), $order_id, $item_id, $ticket_number, $download_token);
 
+    require_once BIXXS_EVENTS_PLUGIN_DIR . '/admin/views/briefkopf/pdf_fields_options.php';
+
+    if ($show_pdf_qrcode) {
+        $qr_code_img = bixxs_events_email_setting_generate_qr_code_url($ticket_number, $guest_number, $ticket_template_id, $ticket_name, $order_date, $ticket_price, $veranstalter, $ort_veranstaltung, $qrcode_color, $guests);
+    } else {
+        $ean_generator = new Picqer\Barcode\BarcodeGeneratorJPG();
+
+        $qr_code_img = BIXXS_EVENTS_PLUGIN_DIR . '/temp/' . $item_id . $ticket_name . $ticket_number . '.jpg';
+
+        file_put_contents($qr_code_img, $ean_generator->getBarcode($ticket_number, $ean_generator::TYPE_CODE_128));
+    }
+
     $replacements = array(
         '[name]' => $first_name . ' ' . $last_name,
         '[first_name]' => $first_name,
@@ -170,7 +225,7 @@ function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, 
         '[veranstalter]' => $veranstalter,
         '[ort_veranstaltung]' => $ort_veranstaltung,
         '[ticketnummer]' => $ticket_number,
-        '[url_download_ticket]' => $download_ticket_url
+        '[url_download_ticket]' => '<a href="' . $download_ticket_url . '" target="_blank">Download</a>',
     );
 
     $subject = $mlx_email_options[$type]['subject'];
@@ -187,14 +242,17 @@ function bixxs_events_send_email($type, WC_Order_Item $item, $guest_number = 1, 
         'From: ' . bloginfo('name') . ' <' . get_option('woocommerce_email_from_address') . '>'
     );
 
-    wp_mail($order->get_billing_email(), $subject, $body, $headers);
+    $attachments = ($type == 'download_ticket') ? array($qr_code_img) : array();
+
+
+    wp_mail($order->get_billing_email(), $subject, $body, $headers, $attachments);
 
     if ($send_to_admin) {
-        wp_mail(get_option('admin_email'), $subject, $body, $headers);
+        wp_mail(get_option('admin_email'), $subject, $body, $headers, $attachments);
     }
 
     if (!empty($guest_email) && $guest_email != $order->get_billing_email()) {
-        wp_mail($guest_email, $subject, $body, $headers);
+        wp_mail($guest_email, $subject, $body, $headers, $attachments);
     }
 }
 
@@ -205,16 +263,8 @@ function bixxs_events_send_initial_email($order_id)
     $order = wc_get_order($order_id);
     $items = $order->get_items();
 
-    error_log(print_r($items, true));
-
-    // add_filter('wp_mail_content_type', function ($content_type) {
-    //     return 'text/html';
-    // });
-
     foreach ($items as $item) {
         $order_item = new WC_Order_Item_Product($item->get_id());
-
-        // error_log(print_r($order_item->get_product()->get_type(), true));
 
         if ($order_item->get_product()->get_type() == 'bixxs_events_product') {
             $guests = json_decode($item->get_meta('_mlx_guests'), true);
@@ -222,7 +272,7 @@ function bixxs_events_send_initial_email($order_id)
             bixxs_events_send_email('buy_ticket', $item);
 
             foreach ($guests as $key => $guest) {
-                bixxs_events_send_email('download_ticket', $item, $key, $guest['email'], false);
+                bixxs_events_send_email('download_ticket', $item, $key, false);
             }
         }
     }
